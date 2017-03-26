485e4218a4a56b19202ddba90eaab9716416f615
rajat-nair <rajat.nair@hpe.com>
2017-03-27 01:54:45 +0530

    Implementing Database check UI

---
+++ Database.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="Database" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Database.aspx.cs" Inherits="DockerSampleWebApplication.Database" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %></h2>
+    <hr />
+    <h4>Database Connection Test</h4>
+    <div class="row">
+        <div class="col-md-4">
+            <asp:Label runat="server" Text="Choose Provider:"></asp:Label>
+            <asp:DropDownList runat="server" AutoPostBack="true" ID="dbConnectionProvider" OnSelectedIndexChanged="dbConnectionProvider_SelectedIndexChanged">
+                <asp:ListItem Selected="True" Text="MS SQL Server" Value="MSSQL"></asp:ListItem>
+                <asp:ListItem Text="Postgres" Value="POSTGRES"></asp:ListItem>
+            </asp:DropDownList>
+        </div>
+        <div class="col-md-8">
+            <asp:Label runat="server" Text="Enter Connection String:"></asp:Label>
+            <asp:TextBox runat="server" TextMode="SingleLine" Width="800px" Text="Data Source=190.190.200.100,1433;Network Library=DBMSSOCN;Initial Catalog=myDataBase;User ID=myUsername;Password=myPassword;" ID="connectionString"></asp:TextBox>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <asp:Button class="btn btn-default" runat="server" Text="Check Connection" OnClick="checkDBConnection_Click" />
+        </div>
+    </div>
+    <hr />
+    <div class="row">
+        <div class="col-md-4">
+            <asp:Label runat="server" Text="Console:"></asp:Label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <asp:TextBox runat="server" ID="outputConsole" Columns="70" Wrap="true" Rows="10" TextMode="MultiLine" Width="800px" Text="Console" ></asp:TextBox>
+        </div>
+    </div>
+</asp:Content>
---
+++ Database.aspx.cs
@@ -0,0 +1,81 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DockerSampleWebApplication
+{
+    public partial class Database : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void checkDBConnection_Click(object sender, EventArgs e)
+        {
+            outputConsole.Text = String.Empty;
+            if (dbConnectionProvider.SelectedValue == "MSSQL")
+            {
+                checkSQLDbConnection(connectionString.Text);
+            }
+            else
+            {
+                checkPostgresConnection(connectionString.Text);
+            }
+        }
+
+        private Boolean checkSQLDbConnection(string connectionString)
+        {
+            Boolean connectionStatus = false;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                }
+                writeToConsole("DB Connection Successful");
+            }
+            catch (Exception ex)
+            {
+                writeToConsole(ex.Message);
+            }
+            return connectionStatus;
+        }
+
+        private Boolean checkPostgresConnection(string connectionString)
+        {
+            Boolean connectionStatus = false;
+            try
+            {
+                using (var conn = new NpgsqlConnection(connectionString))
+                {
+                    conn.Open();
+                }
+                writeToConsole("DB Connection Successful");
+            }
+            catch (Exception ex)
+            {
+                writeToConsole(ex.Message);
+            }
+            return connectionStatus;
+        }
+
+        private void writeToConsole(string message)
+        {
+            outputConsole.Text = $"{outputConsole.Text}\n{message}";
+        }
+
+        protected void dbConnectionProvider_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (dbConnectionProvider.SelectedValue == "POSTGRES")
+                connectionString.Text = "Server=127.0.0.1;Port=5432;Database=postgres;User Id=postgres; Password=postgres;";
+            else
+                connectionString.Text = "Data Source=190.190.200.100,1433;Network Library=DBMSSOCN;Initial Catalog=myDataBase;User ID=myUsername;Password=myPassword;";
+        }
+    }
+}
